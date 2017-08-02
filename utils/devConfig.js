8b71a7e94a8d396e88c018b49d58b81fe01ae3de
simvolice <simvolice@gmail.com>
2017-08-03 03:19:26 +0600

    Настроил проект для запуска и в дев и в прод

--- utils/devConfig.js
+++ utils/devConfig.js
@@ -5,19 +5,21 @@
 
 
 
+
+if (process.env.DEPLOY === 'dev') {
+
+
+
     module.exports = {
 
 
 
 
         port: 3000,
-
-
-        //TODO Надо найти новый сервак, на этом кончились письма
         smtpServer: "smtps://postmaster@mx.efflife.kz:14e25c814c385a93e651c940a9bf5900@smtp.mailgun.org:465",
-
         urlToMongoDBLocalhost: "mongodb://127.0.0.1:27017/efform",
         SECRETJSONWEBTOKEN: "5df9ed11-2bc3-4a2e-a4fc-780c271b25ac",
+
 
         firstStart: false,
 
@@ -28,15 +30,39 @@
 
 
 
+    };
 
 
 
+} else if (process.env.DEPLOY === 'prod'){
+
+
+    module.exports = {
+
+        port: 3065,
+        smtpServer: "smtps://postmaster@mx.efflife.kz:14e25c814c385a93e651c940a9bf5900@smtp.mailgun.org:465",
+        urlToMongoDBLocalhost: "mongodb://admin:1989aaaAAA@127.0.0.1:27017/efform",
+        SECRETJSONWEBTOKEN: "5df9ed11-2bc3-4a2e-a4fc-780c271b25ac",
+
+
+        firstStart: true,
+
+        hashAdmin: "$2a$10$/btJrdH5SXPxIuII9jiUg.JpgRkmJa2Y4A8TbX/X1KRciTVkx4waG"
 
 
 
     };
 
 
+
+
+} else {
+
+
+    console.log("\x1b[41m", new Error("Not set deploy ENV, set env: DEPLOY"));
+    process.exit(1);
+
+}
 
 
 
